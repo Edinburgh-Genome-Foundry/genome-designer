d79b6964e09b300b70d847cbe6ea4f968d08487e
Duncan Meech <duncanmeech@gmail.com>
2015-10-27 13:34:52 -0700

    add scaling and dragging of nodes

--- src/containers/SceneGraphPage.js
+++ src/containers/SceneGraphPage.js
@@ -8,17 +8,23 @@ import Node from '../viewgraph/node';
 import SmartConnector from '../viewgraph/smartconnector';
 import G from '../viewgraph/geometry';
 import Connection from '../viewgraph/connection';
+import DragUI from '../viewgraph/dragui';
 
 // scene graph node types
 
 // scene graph CSS
 import '../css/scenegraph.css';
 
-export class SceneGraphPage extends Component {constructor (props) {
-    super(props);
+export class SceneGraphPage extends Component {
 
+  constructor (props) {
+    super(props);
+    this.scale = 1.0;
   }
 
+  /*
+   * build the scene after the component mounts
+   */
   componentDidMount () {
 
     // empty scene graph
@@ -36,17 +42,9 @@ export class SceneGraphPage extends Component {constructor (props) {
 
     for (let y = 0; y < Y; y += 1) {
       for (let x = 0; x < X; x += 1) {
-        let n = this.nodes[y * X + x] = new Node(this.sceneGraph);
-        n.set({
-          parent: this.sceneGraph.root,
-          glyph: "Rectangle",
-          geometry: new G.Box(20 + x * 200, 20 + y * 150, 100, 100),
-          fill: 'whitesmoke',
-          stroke: 'dodgerblue',
-          strokeWidth: 2,
-          text: 'Rectangle ' + x + ' ,' + y
-        });
-        n.update();
+        let xp = 20 + x * 200;
+        let yp = 20 + y * 150;
+        let n = this.nodes[y * X + x] = this.makeBox(xp, yp, 100, 100, 'Box ' + x + ', ' + y);
       }
     }
 
@@ -55,55 +53,78 @@ export class SceneGraphPage extends Component {constructor (props) {
       for (let y = 0; y < Y - 1; y += 1) {
         var a = this.nodes[y * X + x];
         var b = this.nodes[(y + 1) * X + x + 1];
-        let c = new SmartConnector(this.sceneGraph);
-        c.set({
-          parent: this.sceneGraph.root,
-          glyph: "SmartConnector",
-          geometry: new G.Box(0, 0, 0, 0),
-          stroke: 'orange',
-          strokeWidth: 2
-        });
-
-        let connection = new Connection(c, new G.Vector2D(), a, new G.Vector2D(1, 0.75));
-        c.setConnection('start', connection);
-
-        connection = new Connection(c, new G.Vector2D(), b, new G.Vector2D(0, 0.25));
-        c.setConnection('end', connection);
-
-        c.update();
+        this.connectBoxes(a, new G.Vector2D(1, 0.75), b, new G.Vector2D(0, 0.25), 'orange');
 
         b = this.nodes[y * X + x + 1];
-        c = new SmartConnector(this.sceneGraph);
-        c.set({
-          parent: this.sceneGraph.root,
-          glyph: "SmartConnector",
-          geometry: new G.Box(0, 0, 0, 0),
-          stroke: 'red',
-          strokeWidth: 2
-        });
+        this.connectBoxes(a, new G.Vector2D(0.5, 1), b, new G.Vector2D(0.5, 1), 'red');
 
-        connection = new Connection(c, new G.Vector2D(), a, new G.Vector2D(0.5, 1));
-        c.setConnection('start', connection);
-
-        connection = new Connection(c, new G.Vector2D(), b, new G.Vector2D(0.5, 1));
-        c.setConnection('end', connection);
-
-        c.update();
       }
     }
 
+    // render everything
     this.sceneGraph
       .root
       .updateBranch();
 
+    // add the ui
+    this.ui = new DragUI(this.sceneGraph);
+  }
+
+  /**
+   * helper function for making a box node
+   */
+  makeBox (x, y, w, h, text) {
+    let n = new Node(this.sceneGraph);
+    n.set({
+      parent: this.sceneGraph.root,
+      glyph: "Rectangle",
+      geometry: new G.Box(x, y, w, h),
+      fill: 'whitesmoke',
+      stroke: 'dodgerblue',
+      strokeWidth: 2,
+      text: text
+    });
+    return n;
+  }
+
+  /**
+   * helper for creating connections between boxes
+   */
+  connectBoxes (src, srcPos, dst, dstPos, color) {
+    let c = new SmartConnector(this.sceneGraph);
+    c.set({
+      parent: this.sceneGraph.root,
+      glyph: "SmartConnector",
+      geometry: new G.Box(0, 0, 0, 0),
+      stroke: color,
+      strokeWidth: 2
+    });
+
+    let connection = new Connection(c, new G.Vector2D(), src, srcPos);
+    c.setConnection('start', connection);
+
+    connection = new Connection(c, new G.Vector2D(), dst, dstPos);
+    c.setConnection('end', connection);
+
+    return c;
   }
 
   render () {
     return (
       <div>
+        <br></br>
+        <input type="range" min="0.5" max="4" step="0.05" onChange={this.onScaleChanged.bind(this)}></input>
+        <br></br>
         <div className="scene-graph-container" ref="sceneGraphContainer"></div>
       </div>
     );
+  }
+
+  onScaleChanged (e) {
+    this.scale = parseFloat(e.target.value);
+    this.sceneGraph.setScale(this.scale);
+    this.sceneGraph.root.updateBranch();
+    console.log(this.scale);
   }
 }
 
