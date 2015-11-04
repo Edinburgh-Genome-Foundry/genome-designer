50713498896d1f5a7e671fd4f7ca84a9f556523d
Deepak Chandran <deepak.chandran@autodesk.com>
2015-11-03 18:13:14 -0800

    redis get/set sort of works but some bugs in json format

--- api/index.js
+++ api/index.js
@@ -1,18 +1,50 @@
-import BlockDefinition from '../../src/schemas/Block';
-import ProjectDefinition from '../../src/schemas/Project';
-import PartDefinition from '../../src/schemas/Part';
+import BlockDefinition from '../src/schemas/Block';
+import ProjectDefinition from '../src/schemas/Project';
+import PartDefinition from '../src/schemas/Part';
 
 var path = require('path');
 var express = require('express');
 var uuid = require('uuid');
-var execSync = require('exec-sync');
+var execSync = require('sync-exec');
 var router = express.Router();
+
+/*
+fetch('/api/project?id=f154171f-b158-48eb-a11f-6cddeaecea12',
+   { 
+      method:'get',
+      type:'json'
+    })
+.then(function(res) 
+   { 
+      return res.json() 
+   })
+.then(function(res) 
+   { 
+      console.log(res); 
+   })
+
+
+fetch('/api/project',
+   { 
+      method:'put',
+      body:'{name:"jack"}'
+    })
+.then(function(res) 
+   { 
+      return res.json() 
+   })
+.then(function(res) 
+   { 
+      console.log(res); 
+   })
+*/
 
 router.get('/project', function (req, res) {
   var result = {};
-  console.log(req.query);
   if (req.query.id) {
-    var output = execSync('redis-cli get ' + req.query.id);
+    console.log(req.query);
+    result.id = req.query.id;
+    var output = execSync('redis-cli get ' + req.query.id).stdout;
     if (output.length > 0) {
         try {
             console.log(output);
@@ -29,25 +61,31 @@ router.get('/project', function (req, res) {
 
 router.put('/project', function (req, res) {
   var result = {};
-  if (req.query) {
+  console.log(req.query);
+  req.on('data', function (chunk) {
+    console.log(decodeURI(chunk));
     var id = uuid.v4();
     try {
-        execSync('redis-cli set ' + id + ' ' + JSON.stringify(req.query));
-        result.id = id;    
+        execSync('redis-cli set ' + id + ' ' + chunk);
+        result.id = id;
+        console.log(id);
     } catch (e) {
         result.error = e.message;
         console.log(e);
-    }
-  }
-  res.json(result);
+    }  
+    res.json(result);
+  });
 });
 
 router.post('/project', function (req, res) {
   var result = {};
   console.log(req.query);
+  req.on('data', function (chunk) {
+    console.log(decodeURI(chunk));
+  });
   if (req.query.id) {
     var id = req.query.id;
-    var output = execSync('redis-cli get ' + req.query.id);
+    var output = execSync('redis-cli get ' + req.query.id).stdout;
     if (output.length > 0) {
         try {
             console.log(req.query);
