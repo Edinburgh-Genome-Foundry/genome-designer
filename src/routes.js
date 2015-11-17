97c56f73725b683fce7613846ed1551053a27c49
Duncan Meech <duncanmeech@gmail.com>
2015-11-16 16:35:35 -0800

    initial set of components for rendering a scene graph

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Route, IndexRoute, Redirect } from 'react-router';
+import {
+  Route,
+  IndexRoute,
+  Redirect
+} from 'react-router';
 
 import App from './containers/App';
 import DashboardPage from './containers/DashboardPage';
@@ -9,27 +13,25 @@ import ConstructPage from './containers/ConstructPage';
 import AboutPage from './components/AboutPage';
 import SupportPage from './components/SupportPage';
 
+import SceneGraphPage from './containers/SceneGraphPage';
+
 //Routes are specified as a separate component so they can hotloaded
 //see: https://github.com/rackt/redux-router/issues/44#issuecomment-140198502
 
-export default (
+export default(
   <Route path="/" component={App}>
 
-    {/* todo - signout page w/ dynamic routing *//* todo - signout page w/ dynamic routing */}
+    {/* todo - signout page w/ dynamic routing */
+    /* todo - signout page w/ dynamic routing */}
 
     <IndexRoute component={DashboardPage}/>
 
-    <Route path="/about"
-           component={AboutPage}/>
-    <Route path="/support"
-           component={SupportPage}/>
-
-    <Route path="/project/:projectId"
-           component={ProjectPage}>
-      <Route path="/project/:projectId/:constructId"
-             component={ConstructPage}/>
+    <Route path="/about" component={AboutPage}/>
+    <Route path="/support" component={SupportPage}/>
+    <Route path="/scenegraph" component={SceneGraphPage}/>
+    <Route path="/project/:projectId" component={ProjectPage}>
+      <Route path="/project/:projectId/:constructId" component={ConstructPage}/>
     </Route>
-
-    <Redirect from="/project" to="/" />
+    <Redirect from="/project" to="/"/>
   </Route>
 );
